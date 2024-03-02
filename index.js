fbe3397c322f8bdd02962fe11346a89060feb9da
Prasenjit Basu <144541917+adriyanbasu@users.noreply.github.com>
2024-03-02 22:58:18 +0000

    added a some stuf

--- index.js
+++ index.js
@@ -1,4 +1,33 @@
+const CPU = require("./cpu");
+const creatememory = require("./creatememory");
+const ins = require("./ins");
+
 const memory = 2056 * 1064;
 const writeablebites = new Uint8Array(memory.buffer);
 
-writeablebites[0] = input.charCodeAt(0);
+const cpu = new CPU(creatememory(writeablebites));
+``;
+
+writeablebites[0] = ins.MOV_LIT_R1;
+writeablebites[1] = 0x12;
+writeablebites[2] = 0x34;
+
+writeablebites[3] = ins.MOV_LIT_R2;
+writeablebites[4] = 0xab;
+writeablebites[5] = 0xcd;
+
+writeablebites[6] = ins.ADD_REG_REG;
+writeablebites[7] = 2;
+writeablebites[8] = 3;
+
+cpu.step();
+cpu.debug();
+
+cpu.step();
+cpu.debug();
+
+cpu.step();
+cpu.debug();
+
+cpu.step();
+cpu.debug();
